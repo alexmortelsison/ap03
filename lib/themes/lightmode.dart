528ad309e017fdc9ea978b2e86689a959f42b459
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-06-05 09:02:09 +0900

    updated login_page

---
+++ lib/themes/lightmode.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    surface: Colors.grey.shade300,
+    primary: Colors.grey.shade200,
+    secondary: Colors.white,
+    tertiary: Colors.grey,
+    inversePrimary: Colors.grey.shade700,
+    inverseSurface: Colors.black,
+  ),
+);
